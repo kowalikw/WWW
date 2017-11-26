dd4560cc88c5738b316f45a1eb22a9450a543065
Wojciech Kowalik <kowalikw.mini@gmail.com>
2017-11-26 21:19:47 +0100

    Web app

---
+++ WebApplication1/WebApplication1/Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+    <div id="products">
+    <% for (int i = 0; i < Products.Length; i++) { %>
+        <div class="product">
+            <form method="get" action="Default.aspx">
+                <p class="name"><% =Products[i].Name %></p>
+                <p class="price"><% =Products[i].Price %> zł</p>
+                <input type="number" name="quantity" value="0" />
+                <input type="hidden" name="action" value="add" />
+                <input type="hidden" name="id" value="<% =i %>" />
+                <input type="submit" value="Add to cart" />
+            </form>
+        </div>
+    <% } %>
+    </div>
+    <div id="cart">
+        k
+    </div>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/Warehouse.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApplication1
+{
+    public struct Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    public class Warehouse
+    {
+        public static readonly Product[] PRODUCTS = new Product[]
+        {
+            new Product() { Name = "Laptop", Price = 2999.0 },
+            new Product() { Name = "Powerbank", Price = 64.9 },
+            new Product() { Name = "Mobile Phone", Price = 1599.0 },
+            new Product() { Name = "Sport Camera", Price = 2399.0 },
+            new Product() { Name = "Speakers", Price = 199.0 },
+            new Product() { Name = "Printer", Price = 349.0 },
+            new Product() { Name = "Keyboard", Price = 149.9 },
+            new Product() { Name = "Mouse", Price = 49.9 },
+            new Product() { Name = "Headphones", Price = 129.9 },
+            new Product() { Name = "Drone", Price = 3899 },
+            new Product() { Name = "Digital camera", Price = 999 }
+        };
+    }
+}
